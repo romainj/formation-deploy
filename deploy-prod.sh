2d3f00a858c7ab43916d76b4255040cea5779ea2
Romain JARRAUD <romain.jarraud@trainedpeople.com>
2021-12-09 16:30:19 +0100

    Ajout du script de déploiement.

---
+++ deploy-prod.sh
@@ -0,0 +1,20 @@
+
+drush sset system.maintenance_mode 1
+
+drush sql-dump > ./db/db-$(date '+%Y-%m-%d %H:%M:%S').sql
+
+drush cr
+
+git pull origin main
+
+composer install --no-dev
+
+drush cr
+
+drush updb -y
+
+drush cim -y
+
+drush cr
+
+drush sset system.maintenance_mode 0
